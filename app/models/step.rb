e807f1b64a128c87bcad1ba9ed628101aca08f25
Adriana Sucena Santos <adrianasucena@gmail.com>
2012-07-29 20:12:55 +0100

    Create the step model

---
+++ app/models/step.rb
@@ -0,0 +1,3 @@
+class Step < ActiveRecord::Base
+  attr_accessible :description
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120729181821) do
+ActiveRecord::Schema.define(:version => 20120729190859) do
 
   create_table "ingredients", :force => true do |t|
     t.string   "name"
+    t.text     "description"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
+
+  create_table "steps", :force => true do |t|
     t.text     "description"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
