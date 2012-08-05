576eab3c4325d774da0de73fa6afd2ea88fcd398
Adriana Sucena Santos <adrianasucena@gmail.com>
2012-08-05 18:02:48 +0100

    Add associations

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,4 +1,7 @@
 class Ingredient < ActiveRecord::Base
   attr_accessible :description, :name
   validates_presence_of :name
+
+  has_many :recipe_ingredients
+  has_many :recipes, through: :recipe_ingredients
 end
