0f776c55d3d579c81003694fee77d32226365e78
Adriana Sucena Santos <adrianasucena@gmail.com>
2012-08-05 18:37:05 +0100

    Create test cases for the recipe and small syntax correction

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,5 +1,6 @@
 class Recipe < ActiveRecord::Base
   attr_accessible :name
+  validates_presence_of :name
 
   has_many :recipe_ingredients
   has_many :ingredients, through: :recipe_ingredients
--- spec/models/recipe_spec.rb
+++ spec/models/recipe_spec.rb
@@ -1,5 +1,53 @@
 require 'spec_helper'
 
 describe Recipe do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:subject) {
+    Recipe.create(name:'Orange cake')
+  }
+
+  let(:orange) {
+    Ingredient.create(name:'orange')
+  }
+
+  let(:step1) {
+    Step.create(description:'Mix the sugar with the butter')
+  }
+
+  describe 'saving the model' do
+    context 'given a name' do
+      it 'should save the recipe object' do
+        r = Recipe.new(name:'Orange cake')
+        r.save.should be(true)
+      end
+    end
+
+    context 'given a nil name' do
+      it 'should not save the recipe object' do
+        r = Recipe.new(name:nil)
+        r.save.should be(false)
+      end
+    end
+  end
+
+  describe 'getting ingredients' do
+    context 'given a recipe which has ingredients' do
+      it 'should return the ingredients' do
+        subject.ingredients.should eq([])
+        subject.ingredients << orange
+        subject.ingredients.should eq([orange])
+      end
+    end
+  end
+
+  describe 'getting steps' do
+    context 'given a recipe which has steps' do
+      it 'should return the steps' do
+        subject.steps.should eq([])
+        subject.steps << step1
+        subject.steps.should eq([step1])
+      end
+    end
+  end
+
 end
