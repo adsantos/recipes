7de2acf0c7233fd0c75b4784be470c73267de314
Adriana Sucena Santos <adrianasucena@gmail.com>
2012-09-23 17:44:20 +0100

    Refactor and add a link to each recipe

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -8,4 +8,28 @@ module RecipesHelper
     end
   end
 
+  def build_recipe_link(recipe)
+    content_tag :h3 do
+      concat link_to recipe.name, recipe, class: "recipe_link"
+    end
+  end
+
+  def build_recipe_caption(recipe)
+    content_tag(:div, class: "caption") do
+      concat build_recipe_link(recipe)
+      if current_user
+        concat link_to 'Show', recipe, class:"btn"
+        concat link_to 'Edit', edit_recipe_path(recipe), class:"btn"
+        concat link_to 'Remove', recipe, :confirm => 'Are you sure?', :method => :delete, class:"btn"
+      end
+    end
+  end
+
+  def build_recipe_markup(recipe)
+    content_tag :div, class: "thumbnail" do
+      concat image_tag("http://placehold.it/300x200")
+      concat build_recipe_caption(recipe)
+    end
+  end
+
 end
